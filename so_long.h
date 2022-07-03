1cb8dc06e5741c6f0a1e1422e1ad16e3fd207a4d
jklom0326 <jklom0326@gmail.com>
2022-07-03 23:29:33 +0900

    solong_h

--- so_long.h
+++ so_long.h
@@ -6,10 +6,32 @@
 /*   By: shan <shan@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 14:02:17 by shan              #+#    #+#             */
-/*   Updated: 2022/06/29 15:40:23 by shan             ###   ########.fr       */
+/*   Updated: 2022/07/03 23:29:19 by shan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
 # include <unistd.h>
+# include <stdlib.h>
+# include <mlx.h>
+# include <fcntl.h>
+
+typedef struct t_data
+{
+	char	**map
+	int		c;
+	int		p;
+	int		e;
+	int		z;
+	int		i;
+	int		a;
+	int		b;
+	int		moves;
+	void	*img_ground;
+	void	*img_monster_ball;
+	void	*img_graves;
+	void	*img_graves_in;
+	void	*img_tree;
+	void	*img_trainer1;
+	void	*img_trainer2;
